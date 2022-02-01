63cd34db64b5283c5e9bf0d6edb14bb2545ef4b8
Valentyn98 <valentyn.postolenko98@gmail.com>
2022-02-01 11:17:58 +0200

    2

---
+++ src/components/Dog/Dog.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Dog = ({dog:{id,name},dispatch}) => {
+    return (
+        <div>
+            {name}
+            <button onClick={()=>dispatch({type:'DEL_DOG', payload:{id}})}>DELDOG</button>
+
+        </div>
+    );
+};
+
+export default Dog;
