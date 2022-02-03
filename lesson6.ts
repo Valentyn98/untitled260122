08a4aad100572bc76905105d180f523317091da5
Valentyn98 <valentyn.postolenko98@gmail.com>
2022-02-03 22:21:53 +0200

    Добавить Отнять ТайпСкрипт

--- lesson6.ts
+++ lesson6.ts
@@ -1,11 +1,21 @@
-import {IMission} from "./interfaces/user.inerfaces";
-
-const mis:Partial<IMission>={
-    mission_name:"Starlink-15 (v1.0)",
-    launch_date_local: "2020-10-24T11:31:00-04:00",
-    launch_site:{site_name_long:"Cape Canaveral Air Force Station Space Launch Complex 40"},
-    links:{article_link:null,video_link:"https://youtu.be/J442-ti-Dhg"},
-    rocket:{rocket_name:'Falcon 9',first_stage:{cores:[{flight:8,core:{reuse_count:6,status:'unknown'}}]}},
-    second_stage:{payloads:[{payload_type:"Satellite",payload_mass_kg:15400,payload_mass_lbs:33951.2}]}
+const user = {
+    name:"Max",
+    age:18,
+    gender:'male'
 }
-console.log(mis)
+
+function sum(a:number,b:number){
+    return a+b
+}
+function showSum(a:number,b:number){
+    console.log(a + b);
+}
+
+function incAge(someUser:object, inc:number){
+    someUser['age'] += inc
+    return someUser
+}
+
+console.log(sum(1, 2));
+showSum(2,3)
+incAge(user, 2)
